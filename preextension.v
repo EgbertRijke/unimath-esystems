3470b8df8200e83475059a7494be3908b7169bf6
Egbert Rijke <e.m.rijke@gmail.com>
2015-03-10 18:41:45 -0400

    completed construction of eext2

--- preextension.v
+++ preextension.v
@@ -8,10 +8,31 @@ Definition preextensionAlg ( x : ecarrier ) : UU :=
     ( fun e0 : eF x -> eC x =>
         ( total2 
             ( fun e1 : hfp e0 (eft x) -> eF x =>
-                forall ( P : hfp e0 (eft x) ), eft x (e1 P) = eft x (pr1 (pr1 P))
+                dirprod
+                  ( forall ( P : hfp e0 (eft x) ), 
+                      eft x (e1 P) = eft x (pr1 (pr1 P))
+                  )
+                  ( forall ( P : hfp e0 (eft x) ),
+                      e0 (e1 P) = e0 (pr2 (pr1 P))
+                  )
             )
         )
     ).
+
+Definition preextensionAlg_construct ( x : ecarrier ) ( e0 : eF x -> eC x )
+           ( e1 : hfp e0 (eft x) -> eF x ) 
+           ( codeq : forall ( P : hfp e0 (eft x) ), eft x (e1 P) = eft x (pr1 (pr1 P)) )
+           ( domeq : forall ( P : hfp e0 (eft x) ), e0 (e1 P) = e0 (pr2 (pr1 P)))
+           : preextensionAlg x.
+  Proof.
+  refine (tpair _ _ _).
+  exact e0.
+  refine (tpair _ _ _).
+  exact e1.
+  refine (tpair _ _ _); intro P.
+  exact (codeq P).
+  exact (domeq P).
+  Qed.
 
 Definition ectxext { x : ecarrier } ( CFT : preextensionAlg x ) : eF x -> eC x :=
   pr1 CFT.
@@ -25,36 +46,51 @@ Definition efamext { x : ecarrier } ( CFT : preextensionAlg x ) : eF2 CFT -> eF 
 Definition eft2 { x : ecarrier } ( CFT : preextensionAlg x ) : eF2 CFT -> eF x :=
   fun AP => pr1 (pr1 AP).
 
+Definition eextcodeq { x : ecarrier } ( CFT : preextensionAlg x) ( P : eF2 CFT ) :
+  eft x (efamext CFT P) = eft x (eft2 CFT P) :=
+  (pr1 (pr2 (pr2 CFT))) P.
+ 
+Definition eextdomeq { x : ecarrier } ( CFT : preextensionAlg x) ( P : eF2 CFT ) :
+  ectxext CFT (efamext CFT P) = ectxext CFT (pr2 (pr1 (P))) :=
+  (pr2 (pr2 (pr2 CFT))) P.
+
 Definition eF3 { x : ecarrier } ( CFT : preextensionAlg x ) : UU :=
   hfp (efamext CFT) (eft2 CFT). 
 
 Definition eft3 { x : ecarrier } ( CFT : preextensionAlg x ) : eF3 CFT -> eF2 CFT :=
   fun APQ => pr1 (pr1 APQ).
 
-Print total2.
-Print dirprod.
-Print pr1hSet.
-Print hfp.
-
-About paths.
-
 Definition emixpr2 { x : ecarrier } ( CFT : preextensionAlg x ) (APQ : eF3 CFT) : eF2 CFT.
   Proof.
     destruct APQ as [[[[A P] s0] [[AP Q] s1]] s2].
-    apply ( tpair
-              ( fun xx' : dirprod (eF x) (eF x) =>
-                  eft x (pr2 xx') = ectxext CFT (pr1 xx'))
-              ( tpair (fun _ => eF x) P Q )
-          ).
-    simpl.
-    simpl in s0. simpl in s1. unfold eft2 in s2. simpl in s2.
+    refine (tpair _ _ _).
+    refine (tpair _ _ _).
+    exact P. exact Q.
+    simpl in *.
     rewrite s1.
+    unfold eft2 in s2. simpl in *.
     rewrite s2.
+    refine (eextdomeq CFT _).
+  Qed.
 
-(*
-Definition eext2 {CFT : ecarrier } ( CFTee : preextensionAlg CFT ) : eF3 CFTee -> eF2 CFTee :=
-  fun APQ =>
-    tpair (pr2 (pr1 (pr1 APQ))) (pr2 (pr2 (pr1 APQ))).
-
-About hfp.
-*)
+Definition eext2 { x : ecarrier } ( CFT : preextensionAlg x ) : eF3 CFT -> eF2 CFT.
+  intros [[[[A P] s0] [[AP Q] s1]] s2].
+  refine (tpair _ _ _).
+  refine (tpair _ _ _).
+  exact A.
+  refine (efamext CFT _).
+  refine (tpair _ _ _).
+  refine (tpair _ _ _).
+  exact P.
+  exact Q.
+  unfold eft2 in s2.
+  simpl in *.
+  rewrite s1.
+  rewrite s2.
+  refine (eextdomeq CFT _).
+  unfold eft2 in *.
+  simpl in *.
+  rewrite (eextcodeq CFT _).
+  unfold eft2. simpl.
+  exact s0.
+  Qed.
